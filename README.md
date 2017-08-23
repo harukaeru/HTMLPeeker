b5afdd28bf4773ea690f25725cfd66aa23995db3
Nakada Takumi <usrNeko@kerorin.local>
2017-08-23 16:50:48 +0900

    add to pypi

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # HTMLPeeker
 
+## Requirements
+- Mac OS X
+- Chrome
+- Python3.x (Maybe it can be installed with python2.x)
+
+## Install
+
+```sh
+$ pip install htmlpeeker
+```
+
 ## Usage
+
+### On Command Line
+
+```sh
+$ python3 -c 'import sys; from peeker import peek; peek(sys.argv[1])' "<h1>xxx</h1>"
+```
+
+### As a Library
 
 ```python
 >>> from peeker import peek
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='htmlpeeker',
+    version='0.1',
+    description='Peek html:)',
+    url='https://github.com/harukaeru/HTMLPeeker',
+    author='harukaeru',
+    author_email='nekoneko.myaomyao@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
