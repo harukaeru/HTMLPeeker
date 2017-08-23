90fb2134c04dc7d0dee436474c19adfb8fd9d62e
Nakada Takumi <usrNeko@kerorin.local>
2017-08-23 17:43:34 +0900

    For Vagnrat

--- peeker.py
+++ peeker.py
@@ -2,15 +2,16 @@ import os
 import random
 import subprocess
 import time
+import http.server
 
 
 MAC_CHROME = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
 
 class Peeker:
-    def __init__(self, ext='.html'):
+    def __init__(self, prefix='kaeru_', ext='.html'):
         self.tempfile_name = (
-            'kaeru_' + ''.join(random.sample('harukaeru0123456789', 9)) + ext
+            prefix + ''.join(random.sample('harukaeru0123456789', 9)) + ext
         )
 
     def write_in_tempfile(self, text):
@@ -20,20 +21,44 @@ class Peeker:
     def delete_tempfile(self):
         os.remove(self.tempfile_name)
 
-    def open_chrome(self):
+    def open(self):
         subprocess.run([MAC_CHROME, self.tempfile_name])
 
+    def message(self):
+        print('Start Chrome')
 
-def peek(text):
+
+class FixPeeker(Peeker):
+    """Using http server. This class is used in Vagrant"""
+
+    def __init__(self, prefix='kaeru_fixpeekertemp', ext='.html'):
+        self.tempfile_name = prefix + ext
+
+    def open(self):
+        httpd = http.server.HTTPServer(
+            ("0.0.0.0", 9000), http.server.SimpleHTTPRequestHandler
+        )
+        httpd.serve_forever()
+
+    def message(self):
+        location = f"http://localhost:9000/{self.tempfile_name}"
+        print(f"Open Browser and Check {location}")
+
+
+def peek(text, peeker_class=Peeker):
     if text == '':
         print('No text')
-    peeker = Peeker()
+    peeker = peeker_class()
     try:
         peeker.write_in_tempfile(text)
-        peeker.open_chrome()
-        print('Start Chrome')
+        peeker.message()
+        peeker.open()
         while 1:
             time.sleep(1)
             pass
     except KeyboardInterrupt:
         peeker.delete_tempfile()
+
+
+def fixpeek(text):
+    peek(text, peeker_class=FixPeeker)
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name='htmlpeeker',
-    version='0.1',
+    version='0.1.2',
     description='Peek html:)',
     url='https://github.com/harukaeru/HTMLPeeker',
     author='harukaeru',
     author_email='nekoneko.myaomyao@gmail.com',
     license='MIT',
-    packages=find_packages(),
+    py_modules=['peeker']
 )
